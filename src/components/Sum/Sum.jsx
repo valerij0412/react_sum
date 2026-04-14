4807622c613ceaa8f26e2a7a731794ec4d62bee6
valerij <valerijkolpacov@gmail.com>
2026-04-14 21:18:33 +0300

    REACT_SUM1304

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -3,10 +3,5 @@ import React from 'react';
 export const Sum = ({ a = 0, b = 0 }) => {
   const result = a + b;
 
-  return (
-    <p>
-      {}
-      {`Sum of ${a} and ${b} is ${result}`}
-    </p>
-  );
+  return <p>{`Sum of ${a} and ${b} is ${result}`}</p>;
 };
